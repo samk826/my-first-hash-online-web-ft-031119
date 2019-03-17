0203fb7528cd37e8e352733a98c15832b6464400
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-17 18:53:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {"sam" => 21 }
+  my_hash = {"sam" => 21}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
 
