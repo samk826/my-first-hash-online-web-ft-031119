1d86a1ba327e5a5a4a7a8a40bf436059665a25c8
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-17 18:56:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-  shipping_manifest["muskets"] = 2, ["gun powder"] =4
+  shipping_manifest["muskets"] = 2
 
 shipping_manifest
 end
